0727ed3b5713a1030820855230061fa9288408ec
anas <anasben3issa@gmail.com>
2022-12-17 14:54:39 +0100

    added routes in the header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,22 +1,47 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li>
-                    <router-link class="nav-item nav-link active" to="/">Home</router-link>
+                <li class="nav-item">
+                    <router-link class="nav-link active" to="/">Home</router-link>
                 </li>
-                <li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/books">Books</router-link>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navBarDropDown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">Admin</a>
+                    <ul class="dropdown-menu" aria-labelledby="navBarDropDown">
+                        <li>
+                            <router-link class="dropdown-item" to="/admin/users">Manage Users</router-link>
+                        </li>
+                        <li>
+                            <router-link class="dropdown-item" to="/admin/users/0">Add User</router-link>
+                        </li>
+                        <li>
+                            <router-link class="dropdown-item" to="/admin/books">Manage Books</router-link>
+                        </li>
+                        <li>
+                            <router-link class="dropdown-item" :to="{ name: 'BookEdit', params: { bookId: 0 } }">Add
+                                Book</router-link>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="nav-item">
                     <router-link v-if="store.token == ''" class="nav-item nav-link" to="/login">Login</router-link>
                     <a href="javascript:void(0)" v-else class="nav-item nav-link" @click="logout">Logout</a>
                 </li>
             </ul>
 
             <span class="navbar-text">
-                {{ store.user.first_name ?? '' }} 
+                {{ store.user.first_name ?? '' }}
             </span>
         </div>
     </nav>
@@ -42,20 +67,20 @@ export default {
                 body: JSON.stringify(payload),
             }
             fetch("http://localhost:8081/users/logout", requestOptions)
-            .then((response) => response.json())
-            .then((response) => {
-                if ( response.error){
-                    console.log(response.message);
-                } else {
-                    store.token = "";
-                    store.user = {};
+                .then((response) => response.json())
+                .then((response) => {
+                    if (response.error) {
+                        console.log(response.message);
+                    } else {
+                        store.token = "";
+                        store.user = {};
 
-                    document.cookie = '_site_data=; Path=/; '+
-                    'SameSite=Strict; Secure; ' +
-                    'Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-                    router.push("/login");
-                }
-            })
+                        document.cookie = '_site_data=; Path=/; ' +
+                            'SameSite=Strict; Secure; ' +
+                            'Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+                        router.push("/login");
+                    }
+                })
         }
     }
 }
