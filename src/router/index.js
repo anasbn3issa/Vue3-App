f91bd745bb0129d29fd74d97d8d87e4db4a8bdbb
anas <anasben3issa@gmail.com>
2022-12-17 14:31:09 +0100

    added skeleton components for users and books

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import BodyComponent from "../components/BodyComponent.vue";
 import LoginScreen from "../components/LoginScreen.vue";
+import BooksComponent from "../components/BooksComponent.vue";
+import BookComponent from "../components/BookComponent.vue";
+import BooksAdmin from "../components/BooksAdmin.vue";
+import BookEdit from "../components/BookEdit.vue";
+import UsersComponent from "../components/UsersComponent.vue";
+import UserEdit from "../components/UserEdit.vue";
 
 const routes = [
   {
@@ -12,6 +18,36 @@ const routes = [
     path: "/login",
     name: "Login",
     component: LoginScreen,
+  },
+  {
+    path: "/books",
+    name: "Books",
+    component: BooksComponent,
+  },
+  {
+    path: "/books/:bookName",
+    name: "Books",
+    component: BookComponent,
+  },
+  {
+    path: "/admin/books",
+    name: "BooksAdmin",
+    component: BooksAdmin,
+  },
+  {
+    path: "/admin/books/:bookId",
+    name: "BookEdit",
+    component: BookEdit,
+  },
+  {
+    path: "/admin/users/",
+    name: "Users",
+    component: UsersComponent,
+  },
+  {
+    path: "/admin/users/:userId",
+    name: "User",
+    component: UserEdit,
   }
 ];
 
